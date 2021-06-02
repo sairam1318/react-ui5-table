7169825fce972d78a5119ed1f38a5a317037ed89
Sai Ram <sairam5074@gmail.com>
2021-06-02 18:09:54 +0530

    Update react

---
+++ src/Table.js
@@ -0,0 +1,58 @@
+import { Table, TableColumn, Label, TableCell, TableRow } from "@ui5/webcomponents-react";
+import { useEffect, useState } from 'react';
+export const MyTable = (props) => {
+    const [topics, setTopics] = useState([]);
+    const [loading, setLoading] = useState(true)
+    useEffect(()=> {
+        fetch('https://60b7316917d1dc0017b89431.mockapi.io/users')
+            .then(response => response.json())
+            .then(json => setTopics(json));
+        setLoading(false);
+    }, []);
+  return (
+    <>
+      <Table
+        className=""
+        columns={
+          <>
+            <TableColumn >
+              <Label>ID</Label>
+            </TableColumn>
+            <TableColumn >
+              <Label>Name</Label>
+            </TableColumn>
+            <TableColumn>
+              <Label>Age</Label>
+            </TableColumn>
+           
+          </>
+        }
+        onLoadMore={function noRefCheck() {}}
+        onPopinChange={function noRefCheck() {}}
+        onRowClick={function noRefCheck() {}}
+        slot=""
+        style={{}}
+        tooltip=""
+      >
+        {}
+
+        {loading ? <p>Loading...</p> : topics.map(topic => {
+            return(
+                 <TableRow>
+                    <TableCell>
+                        <Label>{topic.id}</Label>
+                    </TableCell>
+                    <TableCell>
+                        <Label>{topic.name}</Label>
+                    </TableCell>
+                    <TableCell>
+                        <Label>{topic.Age}</Label>
+                    </TableCell>
+                </TableRow>
+               
+            )
+        })}
+      </Table>
+    </>
+  );
+};
