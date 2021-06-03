be6800379b732de54b8a1232735ab0d98d0ed501
Sai Ram <sairam5074@gmail.com>
2021-06-03 15:57:26 +0530

    updated filter bar

--- src/Table.js
+++ src/Table.js
@@ -3,54 +3,58 @@ import { useEffect, useState } from 'react';
 export const MyTable = (props) => {
     const [wellCompletionTable, setWellCompletionTable] = useState([]);
     const [tableHeaders, setTableHeaders] = useState([]);
-    // const [loading, setLoading] = useState(true)
+  
     useEffect(()=> {
         fetch('https://60b7316917d1dc0017b89431.mockapi.io/users')
             .then(response => response.json())
-            .then(json => setWellCompletionTable(json));
-        // setLoading(false)
+            .then(json => setWellCompletionTable(json))
+            
         fetch('http://localhost:3000/posts')
             .then(response => response.json())
-            .then(json => setTableHeaders(json));
+            .then(json => setTableHeaders(json))
         
     }, []);
+
+    const loadwellCompletionHeader = (header) => {
+      return(
+        <>
+            <TableColumn >
+              <Label>{header.title}</Label>
+            </TableColumn>
+        </>
+      )
+    }
+    const loadWellCompletionTableData = (wellData) => {
+      return(
+        <TableRow style={{width: '12rem', textAlign: "start"}}>
+          <TableCell>
+              <Label>{wellData.wellNumber}</Label>
+          </TableCell>
+          <TableCell>
+            <Label>{wellData.wellCity}</Label>
+          </TableCell>
+          <TableCell>
+              <Label>{wellData.wellCompletionDate}</Label>
+          </TableCell>
+      </TableRow>
+      )
+    }
+
   return (
     <>
       <Table
         className=""
-        columns={
-          <>
-            {tableHeaders.map((header)=>{
-              return (<TableColumn >
-              <Label>{header.title}</Label>
-            </TableColumn>)
-            })}
-           
-          </>
-          
-        }
-        onLoadMore={function noRefCheck() {}}
-        onPopinChange={function noRefCheck() {}}
-        onRowClick={function noRefCheck() {}}
-        slot=""
-        style={{}}
-        tooltip=""
+        columns={<>
+              {tableHeaders.map((header)=>{
+                return loadwellCompletionHeader(header);
+              })}
+             </>
+          }
       >
         {}
-        
         {wellCompletionTable.map(wellData => {
-
-            return( <TableRow style={{width: '12rem', textAlign: "start"}}>
-                    <TableCell>
-                        <Label>{wellData.wellNumber}</Label>
-                    </TableCell>
-                    <TableCell>
-                      <Label>{wellData.wellCity}</Label>
-                    </TableCell>
-                    <TableCell>
-                        <Label>{wellData.wellCompletionDate}</Label>
-                    </TableCell>
-                  </TableRow>
+            return( 
+              loadWellCompletionTableData(wellData)
             )
         })}
       
